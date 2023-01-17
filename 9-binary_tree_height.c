60c147b3c4f211901635fafb2983ae5bfbaf4ca2
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-17 13:30:25 -0500

    Solved error from task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_r;
 
 	if (!tree)
-		return( 0);
+		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
