37455b3f34c6ffc99b6a0a458560ce8776b26933
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-16 20:22:34 -0500

    Task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(parent));
+	new_node = malloc(sizeof(*parent));
 
 	if (!new_node)
 		return (NULL);
