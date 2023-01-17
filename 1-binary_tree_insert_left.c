953657e4d7fab5e31f403071363482b2be0a55e4
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-16 22:44:05 -0500

    Task 1 and 2

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+*binary_tree_node - Function to insert the left node in a binary tree
+*@parent: Parent node to add
+*@value: Value of the node
+*Return: Binary tree with left node inserted
+*/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *new_node;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+*binary_tree_node - Function to insert the right node in a binary tree
+*@parent: Parent node to add
+*@value: Value of the node
+*Return: Binary tree with right node inserted
+*/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *new_node;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+
+	if (parent->right)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
+	parent->right = new_node;
+
+	return (new_node);
+}
