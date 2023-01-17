e99584fc9645696089a9d8f99d1aee73493146bf
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-17 13:29:36 -0500

    Solved error from task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,16 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- *
- * @tree: Pointer to the node to measures the height
- *
- * Return: The height of the tree starting at @node
- */
+* binary_tree_height - Measures the height of a binary tree
+* @tree: Node to measure
+* Return: The height of the tree
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_l;
 	size_t height_r;
+
+	if (!tree)
+		return( 0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
