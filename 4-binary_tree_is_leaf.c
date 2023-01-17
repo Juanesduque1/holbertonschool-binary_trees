c41479c8c20141692a94a543556312ece7613321
ObedRav <obedrayo2356@gmail.com>
2023-01-17 17:16:17 +0000

    Add is leaf function

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - function that checks if a node is a leaf
+ * @node: the node to check
+ * Return: 1 if the node is a leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    return ((!node || node->right || node->left) ? 0 : 1);
+}
