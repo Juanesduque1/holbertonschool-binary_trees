dfa74f38074beb0b34d7c751bd3c1112459dc0a2
ObedRav <obedrayo2356@gmail.com>
2023-01-18 13:46:26 +0000

    Betty style code

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,8 +7,9 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    return ((node == node->parent->left) ? node->parent->right : node->parent->left);
+	return ((node == node->parent->left) ?
+		node->parent->right : node->parent->left);
 }
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,10 +7,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    return ((node == node->parent->left) ? node->parent->right : node->parent->left);
+	return ((node == node->parent->left) ?
+		node->parent->right : node->parent->left);
 }
 
 /**
@@ -20,9 +21,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    return (binary_tree_sibling(node->parent));
+	return (binary_tree_sibling(node->parent));
 }
 
